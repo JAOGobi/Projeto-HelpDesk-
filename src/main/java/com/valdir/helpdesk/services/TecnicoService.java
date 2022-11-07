c79738e42d3af1e89c0f8bf2fbc293bc5bf20540
João Gobi <joao_gobi2012@hotmail.com>
2022-11-07 09:27:09 -0300

    delete para Tecnico implementado

--- src/main/java/com/valdir/helpdesk/services/TecnicoService.java
+++ src/main/java/com/valdir/helpdesk/services/TecnicoService.java
@@ -46,6 +46,16 @@ public class TecnicoService {
 		oldObj = new Tecnico(objDTO);
 		return repository.save(oldObj);
 	}
+	
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+		
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+		}
+		
+		repository.deleteById(id);
+	}
 
 	private void validaPorCpfEEMail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
@@ -58,5 +68,7 @@ public class TecnicoService {
 			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
 		}
 	}
+
+
 
 }
