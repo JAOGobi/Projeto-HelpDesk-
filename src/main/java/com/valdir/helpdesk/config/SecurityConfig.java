e32c22ce0b16ca290f31d0d41203b9000e73e2cb
João Gobi <joao_gobi2012@hotmail.com>
2022-11-11 14:05:27 -0300

    permissão para login na SecurityConfig

--- src/main/java/com/valdir/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/valdir/helpdesk/config/SecurityConfig.java
@@ -42,7 +42,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.cors().and().csrf().disable();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
-		http.authorizeRequests() 
+		http.authorizeRequests()
+		    .antMatchers("/login").permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		
